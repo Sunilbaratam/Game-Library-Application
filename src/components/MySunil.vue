9ff6864aa80bbd9a96cea9088497d0bad1b2e874
Sunil Baratam <sunil.baratam6@gmail.com>
2023-10-28 21:29:51 +0530

    intial commit

---
+++ src/components/MySunil.vue
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "MySunil",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://127.0.0.1:5000/mysunil";
+      axios.get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getResponse();
+  },
+};
+</script>
